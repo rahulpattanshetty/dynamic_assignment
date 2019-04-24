0a65008ba1a8c495dbc4ef19972a0c55a06b745d
rahulpattanshetty <rahul.p@fueblabs.com>
2019-04-24 15:45:07 +0530

    changed the notice message for unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   
   rescue_from CanCan::AccessDenied do
-  	redirect_to root_path, notice:"You are not authorized to access the page"
+  	redirect_to root_path, notice:"You don't have Access Rights"
   end
 
   
